07b44ec2ef229233edc103294d984ffb3dd32708
choward1793 <choward1793@yahoo.com>
2022-01-09 18:02:15 -0600

    setup redux form in searchbar js

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,24 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
-import App from "./components/app";
-import reducers from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+import reducers from './reducers';
 
-import "./style/main.scss";
+const createStoreWithMiddleware = applyMiddleware(thunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
+
+import './style/main.scss';
+
+import Home from './components/home';
+import Results from './components/results';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
       <BrowserRouter>
-        <App />
+        <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/results' component={Results}/>
+        </Switch>
       </BrowserRouter>
-    </Provider>,
-    document.querySelector(".app-wrapper")
-  );
+    </Provider>
+    , document.querySelector('.app-wrapper'));
 }
 
-document.addEventListener("DOMContentLoaded", main);
+document.addEventListener('DOMContentLoaded', main);
