55927474f9bcbe99e4abf651f684e4428c04a3aa
choward1793 <choward1793@yahoo.com>
2022-01-10 13:17:27 -0600

    put together fetch posts with query action

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -4,6 +4,8 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import thunk from 'redux-thunk';
+
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(compose((window.devToolsExtension ? window.devToolsExtension() : f => f)(createStore)));
